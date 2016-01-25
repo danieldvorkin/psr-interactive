506be08f4b1d7f45e8a847cc390503618731bb39
Daniel Dvorkin <dvorkin212@gmail.com>
2016-01-25 10:58:14 -0500

    Cleaned up the js, cleaned up the css, added some transition effects on the index page

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
 $(function(){
   $('.pics').find('> div:eq(0)').nextAll().hide;
 
-  $('.one.column .div-block, .picture-block').on('click', function(){
-    var i = $('.one.column .div-block, .picture-block').index(this);
+  $('.one.column .div-block').on('click', function(){
+    var key = $(this).attr('href').replace("#", "");
 
     $('.div-block').removeClass('active');
     $(this).addClass('active');
 
-    addHide('.pics .active, .content .active, .info .active');
-    addActive('.pics > .hide:eq('+i+'), .content > .hide:eq('+i+'), .info .hide:eq('+i+')', i);
+    $('.pics .active, .content .active').removeClass('active').addClass('hide');
+    $("div[data-id='"+ key +"']").removeClass('hide').addClass('active');
   });
 });
-
-function addHide(selectors){
-  $(selectors).removeClass('active').addClass('hide');
-}
-
-function addActive(selectors, i){
-  $(selectors).removeClass('hide').addClass('active');
-}
