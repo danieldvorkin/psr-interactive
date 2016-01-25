b056876faed1690afe940a9da9dc2345c84a5152
Daniel Dvorkin <dvorkin212@gmail.com>
2016-01-25 15:17:16 -0500

    whole lotta changes made

--- js/sec-script.js
+++ js/sec-script.js
@@ -1,7 +1,12 @@
 $(function(){
   $('.picture-block').on('click', function(){
+    $('.picture-block').removeClass('selected');
+    $(this).addClass('selected');
+
+
     var key = $(this).attr("href").replace("#", "");
     $('.info .active').removeClass('active').addClass('hide');
+
     $("div[data-id='"+key+"']").removeClass('hide').addClass('active');
   });
 });
