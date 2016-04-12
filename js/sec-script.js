3a6fafac467e81ccd956412636a99a7803990865
Daniel Dvorkin <dvorkin212@gmail.com>
2016-04-12 12:33:25 -0400

    updated alot of shit

--- js/sec-script.js
+++ js/sec-script.js
@@ -1,12 +1,30 @@
 $(function(){
+  $('.fun').hide();
+  $('.base').show();
+  $('.fun:first').show();
+  $('.base:first').hide();
+
   $('.picture-block').on('click', function(){
     $('.picture-block').removeClass('selected');
-    $(this).addClass('selected');
+    $('.agent-label').removeClass('selected');
 
+    $('.picture-block .fun').hide();
+    $('.picture-block .base').show();
+
+    $(this).addClass('selected');
+    $(this).children('.agent-label').addClass('selected');
+
+    $(this).children(".fun").show();
+    $(this).children(".base").hide();
 
     var key = $(this).attr("href").replace("#", "");
     $('.info .active').removeClass('active').addClass('hide');
 
     $("div[data-id='"+key+"']").removeClass('hide').addClass('active');
+
+    // This needs to fade the content info
+    // $("div[data-id='"+key+"']").removeClass('hide').fadeIn("slow", function(){
+    //   $(this).addClass('active');
+    // });
   });
 });
